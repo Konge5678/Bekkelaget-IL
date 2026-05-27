cb4761ff867cf214dcba4741bef7d2b964fff5eb
Kristian Haugsrud <kris.haugsrud@gmail.com>
2026-05-27 16:58:50 +0200

    initial: setup next.js project with core dependencies

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Fagprøve Bekkelaget IL</h1>
+    </div>
+  );
+}
